26fe2bbdb1a6289d157f1de8dcc0ece11f68466b
Yago09071 <yagonegrao09@gmail.com>
2024-07-25 20:12:48 -0300

    Adicionar arquivos de projeto.

---
+++ atv/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+class HelloWorld
+{
+    public static void Escreve()
+    {
+        Console.WriteLine(
+            "_|                  _|  _|                                                              _|        _|  _|\r\n" +
+            "_|_|_|      _|_|    _|  _|    _|_|              _|      _|      _|    _|_|    _|  _|_|  _|    _|_|_|  _|\r\n" +
+            "_|    _|  _|_|_|_|  _|  _|  _|    _|            _|      _|      _|  _|    _|  _|_|      _|  _|    _|  _|\r\n" +
+            "_|    _|  _|        _|  _|  _|    _|              _|  _|  _|  _|    _|    _|  _|        _|  _|    _|\r\n" +
+            "_|    _|    _|_|_|  _|  _|    _|_|      _|          _|      _|        _|_|    _|        _|    _|_|_|  _|\r\n" +
+            "                                      _|\r\n\r\n"
+        );
+    }
+}
+
+class Calculadora
+{
+    public static void Inicia()
+    {
+        Console.WriteLine("Selecione a Operação:\n" +
+            "1) Soma\n" +
+            "2) Subtração\n" +
+            "3) Multiplicação\n" +
+            "4) Divisão");
+
+        Console.Write("> ");
+        string op = Console.ReadLine();
+
+        Console.Write("Numero 1: ");
+        float num1 = float.Parse(Console.ReadLine());
+        Console.Write("Numero 2: ");
+        float num2 = float.Parse(Console.ReadLine());
+
+        float? resultado = Calcula(op, num1, num2);
+
+        if (resultado == null)
+        {
+            Console.WriteLine("Operação Inválida");
+            return;
+        }
+
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+
+    public static float? Calcula(string op, float num1, float num2)
+    {
+        return op switch
+        {
+            "1" => num1 + num2,
+            "2" => num1 - num2,
+            "3" => num1 * num2,
+            "4" => num1 / num2,
+            _ => (float?)null
+        };
+    }
+}
+
+class ParImpar
+{
+    public static void Inicia()
+    {
+        Console.Write("Digite um Número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Par!");
+        }
+        else
+        {
+            Console.WriteLine("Ímpar");
+        }
+    }
+}
+
+class Program
+{
+    public static bool deve_sair = false;
+
+    public static void Main(string[] args)
+    {
+        while (!deve_sair)
+        {
+            Console.WriteLine("Escolha um Programa:\n" +
+                "1: Hello World\n" +
+                "2: Calculadora\n" +
+                "3: Par ou Ímpar");
+                
+
+            Console.Write("> ");
+            string escolha = Console.ReadLine();
+
+            if (escolha == "1")
+                HelloWorld.Escreve();
+            if (escolha == "2")
+                Calculadora.Inicia();
+            if (escolha == "3")
+                ParImpar.Inicia();
+           
+        
+        }
+    }
+}
